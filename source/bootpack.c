9a52dedd9d6f24891a7bc99ada5d5f28714060be
axiomaster <jiangshanliuyun@gmail.com>
2016-10-09 18:03:03 +0800

    绘制鼠标

--- source/bootpack.c
+++ source/bootpack.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 void io_hlt(void);
 void io_cli(void);
 void io_out8(int port, int data);
@@ -10,6 +12,9 @@ void set_palette(int start, int end, unsigned char *rgb);
 void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
 void init_screen(char *vram, int x, int y);	//初始化桌面
 void putfont8(char *vram, int xsize, int x, int y, char c, char *font); //写字
+void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s);
+void init_mouse_cursor8(char *mouse, char bc);
+void putblock8_8(char *vram, int vxsize, int pxsize, int pysize, int px0, int py0, char *buf, int bxsize);
 
 #define COL8_000000		0
 #define COL8_FF0000		1
@@ -37,16 +42,19 @@ struct BOOTINFO{
 void HariMain(void)
 {
 	struct BOOTINFO *binfo = (struct BOOTINFO *) 0x0ff0;
-	extern char hankaku[4096];
+    char s[40], mcursor[256];
+    int mx, my;
+
 	init_palette();
 	init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
-	
-	putfont8(binfo->vram, binfo->scrnx,  8, 8, COL8_FFFFFF, hankaku + 'A' * 16);
-	putfont8(binfo->vram, binfo->scrnx, 16, 8, COL8_FFFFFF, hankaku + 'B' * 16);
-	putfont8(binfo->vram, binfo->scrnx, 24, 8, COL8_FFFFFF, hankaku + 'C' * 16);
-	putfont8(binfo->vram, binfo->scrnx, 40, 8, COL8_FFFFFF, hankaku + '1' * 16);
-	putfont8(binfo->vram, binfo->scrnx, 48, 8, COL8_FFFFFF, hankaku + '2' * 16);
-	putfont8(binfo->vram, binfo->scrnx, 56, 8, COL8_FFFFFF, hankaku + '3' * 16);
+
+    mx = (binfo->scrnx-16)/2;
+    my = (binfo->scrny-28-16)/2;
+    init_mouse_cursor8(mcursor, COL8_008484);
+    putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
+
+    sprintf(s, "(%d, %d)", mx, my);
+    putfonts8_asc(binfo->vram, binfo->scrnx, 16, 64, COL8_FFFFFF, s);
 
 fin:					//让cpu休眠
     io_hlt();
@@ -141,4 +149,63 @@ void putfont8(char *vram, int xsize, int x, int y, char c, char *font)
 		if ((d & 0x01) != 0) { p[7] = c; }
 	}
 	return;
+}
+
+void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s)
+{
+	extern char hankaku[4096];
+	for(;*s!=0x00;s++){
+		putfont8(vram, xsize, x, y, c, hankaku+*s*16);
+		x += 8;
+	}
+	return;
+}
+
+void init_mouse_cursor8(char *mouse, char bc)
+{
+    static char cursor[16][16] = {
+            "**************..",
+            "*OOOOOOOOOOO*...",
+            "*OOOOOOOOOO*....",
+            "*OOOOOOOOO*.....",
+            "*OOOOOOOO*......",
+            "*OOOOOOO*.......",
+            "*OOOOOOO*.......",
+            "*OOOOOOOO*......",
+            "*OOOO**OOO*.....",
+            "*OOO*..*OOO*....",
+            "*OO*....*OOO*...",
+            "*O*......*OOO*..",
+            "**........*OOO*.",
+            "*..........*OOO*",
+            "............*OO*",
+            ".............***"
+    };
+    int x, y;
+    for (y = 0; y < 16; y++) {
+        for (x = 0; x < 16; x++) {
+            if (cursor[y][x] == '*') {
+                mouse[y * 16 + x] = COL8_000000;
+            }
+            if (cursor[y][x] == 'O') {
+                mouse[y * 16 + x] = COL8_FFFFFF;
+            }
+            if (cursor[y][x] == '.') {
+                mouse[y * 16 + x] = bc;
+            }
+        }
+    }
+    return;
+}
+
+void putblock8_8(char *vram, int vxsize, int pxsize,
+                 int pysize, int px0, int py0, char *buf, int bxsize)
+{
+    int x, y;
+    for(y = 0;y<pysize;y++){
+        for(x = 0;x<pxsize;x++){
+            vram[(py0+y)*vxsize+(px0+x)] = buf[y*bxsize+x];
+        }
+    }
+    return;
 }
