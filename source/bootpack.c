61e6a02cf926a6e8038833b690e22ac6011ed8a1
axiomaster <jiangshanliuyun@gmail.com>
2016-10-24 14:22:22 +0800

    鼠标移动绘制

--- source/bootpack.c
+++ source/bootpack.c
@@ -1,16 +1,24 @@
 #include "bootpack.h"
 #include <stdio.h>
 
+struct MOUSE_DEC{
+    unsigned char buf[3]; //鼠标中断产生3字节数据
+    unsigned char phase;  //中断数据读取状态
+    int x, y, btn;
+};
+
 extern struct FIFO8 keyfifo;
 extern struct FIFO8 mousefifo;
-void enable_mouse(void);
+void enable_mouse(struct MOUSE_DEC *mdec);
 void init_keyboard(void);
+int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat);
 
 void HariMain(void)
 {
 	struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
     char s[40], mcursor[256], keybuf[32], mousebuf[128];
     int mx, my, i;
+    struct MOUSE_DEC mdec;
 
     init_gdtidt();
     init_pic();
@@ -21,7 +29,7 @@ void HariMain(void)
     io_out8(PIC0_IMR, 0xf9);
     io_out8(PIC1_IMR, 0xef);
 
-    init_keyboaard();
+    init_keyboard();
 
 	init_palette();
 	init_screen8(binfo->vram, binfo->scrnx, binfo->scrny);
@@ -34,7 +42,7 @@ void HariMain(void)
     sprintf(s, "(%d, %d)", mx, my);
     putfonts8_asc(binfo->vram, binfo->scrnx, 16, 64, COL8_FFFFFF, s);
 
-    enable_mouse();
+    enable_mouse(&mdec);
 
     for(;;){
         io_cli();
@@ -50,9 +58,34 @@ void HariMain(void)
             }else{
                 i = fifo8_get(&mousefifo);
                 io_sti();
-                sprintf(s, "%02X", i);
-                boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 16, 15, 31);
-                putfonts8_asc(binfo->vram, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
+                if(mouse_decode(&mdec, i)!=0){
+                    sprintf(s, "[lcr %4d %4d]", mdec.x, mdec.y);
+                    if((mdec.btn & 0x01)!=0){
+                        s[1] = 'L';
+                    }
+                    if((mdec.btn &0x02)!=0){
+                        s[3] = 'R';
+                    }
+                    if((mdec.btn & 0x04)!=0){
+                        s[2] = 'C';
+                    }
+
+                    boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 32+15*8-1, 31);
+                    putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+
+                    boxfill8(binfo->vram, binfo->scrnx, COL8_008484, mx, my, mx+15, my+15);
+                    mx += mdec.x;
+                    my += mdec.y;
+                    if(mx<0) mx = 0;
+                    if(my<0) my = 0;
+                    if(mx>binfo->scrnx-16) mx = binfo->scrnx-16;
+                    if(my>binfo->scrny-16) my = binfo->scrny-16;
+
+                    sprintf(s, "(%3d, %3d)", mx, my);
+                    boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 0, 79, 15);
+                    putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
+                    putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
+                }
             }
         }
     }
@@ -75,7 +108,7 @@ void wait_KBC_sendready(void)
     }
 }
 
-void init_keyboaard(void)
+void init_keyboard(void)
 {
     wait_KBC_sendready();
     io_out8(PORT_KEYCMD, KEYCMD_WRITE_MODE);
@@ -86,10 +119,51 @@ void init_keyboaard(void)
 #define KEYCMD_SENDTO_MOUSE     0xd4
 #define MOUSECMD_ENABLE         0xf4
 
-void enable_mouse(void)
+void enable_mouse(struct MOUSE_DEC *mdec)
 {
     wait_KBC_sendready();
     io_out8(PORT_KEYCMD, KEYCMD_SENDTO_MOUSE);
     wait_KBC_sendready();
     io_out8(PORT_KEYDAT, MOUSECMD_ENABLE);
+
+    mdec->phase = 0;
+    return;
+}
+
+int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat)
+{
+    switch (mdec->phase){
+        case 0:
+            if(dat==0xfa) mdec->phase = 1;
+            return 0;
+            break;
+        case 1:
+            if((dat&0xc8) == 0x08){
+                mdec->buf[0] = dat;
+                mdec->phase = 2;
+            }
+            return 0;
+            break;
+        case 2:
+            mdec->buf[1] = dat;
+            mdec->phase = 3;
+            return 0;
+            break;
+        case 3:
+            mdec->buf[2] = dat;
+            mdec->phase = 1;
+            mdec->btn = mdec->buf[0] & 0x07;
+            mdec->x = mdec->buf[1];
+            mdec->y = mdec->buf[2];
+
+            if((mdec->buf[0] & 0x10)!=0){
+                mdec->x |= 0xffffff00;
+            }
+            if((mdec->buf[0] & 0x20)!=0){
+                mdec->y |= 0xffffff00;
+            }
+            mdec->y = -mdec->y;
+            return 1;
+    }
+    return -1;
 }
