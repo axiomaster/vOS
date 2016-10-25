eeb9c23d89bb484feb36c3c0c2f8b30e29c48271
axiomaster <jiangshanliuyun@gmail.com>
2016-10-25 23:00:37 +0800

    加快图层绘制

--- source/sheet.c
+++ source/sheet.c
@@ -45,6 +45,7 @@ void sheet_setbuf(struct SHEET *sht, unsigned char *buf, int xsize, int ysize, i
 	return;
 }
 //刷新鼠标指针，避免全屏刷新
+/*
 void sheet_refreshsub(struct SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1)
 {
 	int h, bx, by, vx, vy;
@@ -63,6 +64,38 @@ void sheet_refreshsub(struct SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1)
 						vram[vy*ctl->xsize + vx] = c;
 					}
 				}
+			}
+		}
+	}
+	return;
+}
+*/
+
+void sheet_refreshsub(struct SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1)
+{
+	int h, bx, by, vx, vy, bx0, by0, bx1, by1;
+	unsigned char *buf, c, *vram = ctl->vram;
+	struct SHEET *sht;
+	for (h = 0; h <= ctl->top; h++) {
+		sht = ctl->sheets[h];
+		buf = sht->buf;
+
+		bx0 = vx0 - sht->vx0;
+		by0 = vy0 - sht->vy0;
+		bx1 = vx1 - sht->vx0;
+		by1 = vy1 - sht->vy0;
+		if (bx0 < 0) bx0 = 0;
+		if (by0 < 0) by0 = 0;
+		if (bx1 > sht->bxsize) bx1 = sht->bxsize;
+		if (by1 > sht->bysize) by1 = sht->bysize;
+
+		for (by = by0; by < by1; by++) {
+			vy = sht->vy0 + by;
+			for (bx = bx0; bx < bx1; bx++) {
+				vx = sht->vx0 + bx;
+				c = buf[by*sht->bxsize + bx];
+				if (c != sht->col_inv)
+					vram[vy*ctl->xsize + vx] = c;
 			}
 		}
 	}
