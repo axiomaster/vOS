6dba7e740de6959d0dfe60ecccee7e8d5c87558c
axiomaster <jiangshanliuyun@gmail.com>
2016-10-28 10:09:32 +0800

    Revert "多任务管理"
    This reverts commit 5b380da992713469256bc2037839814471375905.

--- source/mtask.c
+++ source/mtask.c
@@ -12,7 +12,7 @@ struct TASK *task_init(struct MEMMAN *memman)
 	for (i = 0; i < MAX_TASKS; i++) {
 		taskctl->tasks0[i].flags = 0;
 		taskctl->tasks0[i].sel = (TASK_GDT0 + i) * 8;
-		set_segmdesc(gdt + TASK_GDT0 + i, 103, (int)&taskctl->tasks0[i].tss, AR_TSS32); //任务的TSS段
+		set_segmdesc(gdt + TASK_GDT0 + i, 103, (int)&taskctl->tasks0[i].tss, AR_TSS32);
 	}
 	task = task_alloc();
 	task->flags = 2; //活动中
@@ -91,7 +91,7 @@ void task_sleep(struct TASK *task)
 		for (; i < taskctl->running; i++) {
 			taskctl->tasks[i] = taskctl->tasks[i + 1];
 		}
-		task->flags = 1; //不工作的状态
+		task->flags = 1;
 		if (ts != 0) { //任务切换
 			if (taskctl->now >= taskctl->running) {
 				taskctl->now = 0;
