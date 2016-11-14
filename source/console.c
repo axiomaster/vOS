b71ea47a30d988744db92132703a8b5e01e32258
DESKTOP-UN7PI8F\lism <jiangshanliuyun@gmail.com>
2016-11-14 09:12:32 +0800

    画线

--- source/console.c
+++ source/console.c
@@ -325,6 +325,57 @@ int cmd_app(struct CONSOLE *cons, int *fat, char *cmdline)
 	return 0;
 }
 
+void hrb_api_linewin(struct SHEET *sht, int x0, int y0, int x1, int y1, int col)
+{
+	int i, x, y, len, dx, dy;
+	dx = x1 - x0;
+	dy = y1 - y0;
+	x = x0 << 10;
+	y = y0 << 10;
+	if (dx < 0) {
+		dx = -dx;
+	}
+	if (dy < 0) {
+		dy = -dy;
+	}
+	if (dx >= dy) {
+		len = dx + 1;
+		if (x0 > x1) {
+			dx = -1024;
+		}
+		else {
+			dx = 1024;
+		}
+		if (y0 <= y1) {
+			dy = ((y1 - y0 + 1) << 10) / len;
+		}
+		else {
+			dy = ((y1 - y0 - 1) << 10) / len;
+		}
+	}
+	else {
+		len = dy + 1;
+		if (y0 > y1) {
+			dy = -1024;
+		}
+		else {
+			dy = 1024;
+		}
+		if (x0 <= x1) {
+			dx = ((x1 - x0 + 1) << 10) / len;
+		}
+		else {
+			dx = ((x1 - x0 - 1) << 10) / len;
+		}
+	}
+	for (i = 0; i < len; i++) {
+		sht->buf[(y >> 10)*sht->bxsize + (x >> 10)] = col;
+		x += dx;
+		y += dy;
+	}
+	return;
+}
+
 int hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int eax)
 {
 	int ds_base = *((int *)0xfe8);
@@ -359,12 +410,16 @@ int hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int e
 	else if (edx == 6) {
 		sht = (struct SHEET *) ebx;
 		putfonts8_asc(sht->buf, sht->bxsize, esi, edi, eax, (char *)ebp + ds_base);
-		sheet_refresh(sht, esi, edi, esi + ecx * 8, edi + 16);
+		if ((ebx & 1) == 0) {
+			sheet_refresh(sht, esi, edi, esi + ecx * 8, edi + 16);
+		}
 	}
 	else if (edx == 7) {
 		sht = (struct SHEET *) ebx;
 		boxfill8(sht->buf, sht->bxsize, ebp, eax, ecx, esi, edi);
-		sheet_refresh(sht, eax, ecx, esi + 1, edi + 1);
+		if ((ebx & 1) == 1) {
+			sheet_refresh(sht, eax, ecx, esi + 1, edi + 1);
+		}
 	}
 	else if (edx == 8) {
 		memman_init((struct MEMMAN *)ebx + ds_base);
@@ -382,7 +437,20 @@ int hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int e
 	else if (edx == 11) {
 		sht = (struct SHEET *)ebx;
 		sht->buf[sht->bxsize*edi + esi] = eax;
-		sheet_refresh(sht, esi, edi, esi + 1, edi + 1);
+		if ((ebx & 1) == 0) {
+			sheet_refresh(sht, esi, edi, esi + 1, edi + 1);
+		}
+	}
+	else if (edx == 12) {
+		sht = (struct SHEET *)ebx;
+		sheet_refresh(sht, eax, ecx, esi, edi);
+	}
+	else if (edx == 13) {
+		sht = (struct SHEET *)(ebx & 0xfffffffe);
+		hrb_api_linewin(sht, eax, ecx, esi, edi, ebp);
+		if ((ebx & 1) == 0) {
+			sheet_refresh(sht, eax, ecx, esi + 1, edi + 1);
+		}
 	}
 	return 0;
 }
