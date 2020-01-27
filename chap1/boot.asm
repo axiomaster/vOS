2c79cb32356919536d765aeaa8c52780ab54c519
axiomaster <jiangshanliuyun@gmail.com>
2020-01-28 01:40:15 +0800

    comment

--- chap1/boot.asm
+++ chap1/boot.asm
@@ -1,9 +1,13 @@
-        org 07c00h
+; nasm boot.asm -o boot.bin
+; dd if=boot.bin of=a.img bs=512 count=1 conv=notrunc
+; qemu-system-x86_64.exe -fda a.img
+org 07c00h
         mov ax, cs
         mov ds, ax
         mov es, ax
         call DispStr
         jmp $
+
 DispStr:
         mov ax, BootMessage
         mov bp, ax
@@ -13,6 +17,7 @@ DispStr:
         mov dl, 0
         int 10h
         ret
+
 BootMessage:        db  "Hello, OS world!"
 times   510-($-$$)  db  0
 dw      0xaa55
